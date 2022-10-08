a6e1e26d1d971a90e9824687b1c5d8a06504fb5a
ni30Kosh <nitish@getkosh.com>
2022-10-08 23:46:59 +0530

    waiting for 1 sec error 1

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,12 +2,12 @@
 Django command to wait for the database to be available
 """
 import time
-from psycopg2 import OperationalError as Psycopg2opError
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
-from symbol import try_stmt
+
 
 
 class Command(BaseCommand):
@@ -20,7 +20,7 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2opError, OperationalError):
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write("Databse unavilable, waiting 1 second...")
                 time.sleep(1)
 
